89a63a874cb05e6b2bf53c19f718a8f99ac85b33
Максим <popovmv92@gmail.com>
2022-03-13 14:27:15 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Maksim Popov");
+        System.out.println("16.06.1992");
+    }
+}
